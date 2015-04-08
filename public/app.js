2a35d5d166cef7d181d4866d81c70bcc19370ca3
Wojciech Szóstak <gwszostak@gmail.com>
2015-04-08 21:22:33 +0200

    New window example

--- public/app.js
+++ public/app.js
@@ -1,13 +1,26 @@
 (function(angular){
     var app = angular.module('PdfPOC', []);
 
-    app.controller('ShowPdfController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
-        $scope.showPdf = function () {        	
-        	$http.get('/response.pdf', {responseType: 'arraybuffer'}).success(function(data){
-        		var blob = new Blob([data], {type: 'application/pdf'});
-        		var url = URL.createObjectURL(blob);
-        		$scope.content = $sce.trustAsResourceUrl(url);
-        	});
+    app.controller('ShowPdfController', ['$scope', '$http', '$sce', '$window', function($scope, $http, $sce, $window){
+
+        var getPdfUrl = function (callback) {
+            $http.get('/response.pdf', {responseType: 'arraybuffer'}).success(function(data){
+                var blob = new Blob([data], {type: 'application/pdf'});
+                var url = URL.createObjectURL(blob);
+                callback(url)
+            });
+        };
+
+        $scope.showPdfPopup = function () {
+            getPdfUrl(function(url){
+                $window.open(url);
+            });
+        }
+
+        $scope.showPdf = function () {
+            getPdfUrl(function(url){
+                $scope.content = $sce.trustAsResourceUrl(url);
+            });
         };
     }])
 }(angular))
